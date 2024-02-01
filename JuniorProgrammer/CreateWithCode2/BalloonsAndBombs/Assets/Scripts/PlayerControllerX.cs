d966483b41dc3ed46663df41dce3ef89ad965d1d
Ahmed Razic <ahmed.razic@gmail.com>
2024-02-01 02:04:45 -0800

    Balloons and bombs finished

--- JuniorProgrammer/CreateWithCode2/BalloonsAndBombs/Assets/Scripts/PlayerControllerX.cs
+++ JuniorProgrammer/CreateWithCode2/BalloonsAndBombs/Assets/Scripts/PlayerControllerX.cs
@@ -8,6 +8,10 @@ public class PlayerControllerX : MonoBehaviour
 
     public float floatForce;
     private float gravityModifier = 1.5f;
+    private float upperBound = 16.0f;
+    private float lowerBound = 7.0f;
+    private bool isLowEnough = true;
+
     private Rigidbody playerRb;
 
     public ParticleSystem explosionParticle;
@@ -16,6 +20,7 @@ public class PlayerControllerX : MonoBehaviour
     private AudioSource playerAudio;
     public AudioClip moneySound;
     public AudioClip explodeSound;
+    public AudioClip bounceSound;
 
 
     // Start is called before the first frame update
@@ -23,6 +28,7 @@ public class PlayerControllerX : MonoBehaviour
     {
         Physics.gravity *= gravityModifier;
         playerAudio = GetComponent<AudioSource>();
+        playerRb = GetComponent<Rigidbody>();
 
         // Apply a small upward force at the start of the game
         playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
@@ -33,9 +39,25 @@ public class PlayerControllerX : MonoBehaviour
     void Update()
     {
         // While space is pressed and player is low enough, float up
-        if (Input.GetKey(KeyCode.Space) && !gameOver)
+        if (Input.GetKey(KeyCode.Space) && !gameOver && isLowEnough)
         {
             playerRb.AddForce(Vector3.up * floatForce);
+        }
+
+        if (transform.position.y > upperBound)
+        {
+            //transform.position = new Vector3(transform.position.x, upperBound, transform.position.z);
+            playerRb.AddForce(Vector3.down * 5, ForceMode.Impulse);
+            playerAudio.PlayOneShot(bounceSound);
+        }
+
+        if (transform.position.y < lowerBound)
+        {
+            isLowEnough = true;
+        }
+        else
+        {
+            isLowEnough = false;
         }
     }
 
@@ -49,7 +71,7 @@ public class PlayerControllerX : MonoBehaviour
             gameOver = true;
             Debug.Log("Game Over!");
             Destroy(other.gameObject);
-        } 
+        }
 
         // if player collides with money, fireworks
         else if (other.gameObject.CompareTag("Money"))
@@ -57,9 +79,13 @@ public class PlayerControllerX : MonoBehaviour
             fireworksParticle.Play();
             playerAudio.PlayOneShot(moneySound, 1.0f);
             Destroy(other.gameObject);
-
         }
 
+        else if (other.gameObject.CompareTag("Ground"))
+        {
+            playerRb.AddForce(Vector3.up * 15, ForceMode.Impulse);
+            playerAudio.PlayOneShot(bounceSound);
+        }
     }
 
 }

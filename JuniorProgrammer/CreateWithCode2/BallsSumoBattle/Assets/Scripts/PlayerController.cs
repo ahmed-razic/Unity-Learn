8e5598cab156fbfd659500d70cbfcea9f14d3822
Ahmed Razic <ahmed.razic@gmail.com>
2024-02-02 02:03:21 -0800

    BallsSummoBattle - Added powerup logic

--- JuniorProgrammer/CreateWithCode2/BallsSumoBattle/Assets/Scripts/PlayerController.cs
+++ JuniorProgrammer/CreateWithCode2/BallsSumoBattle/Assets/Scripts/PlayerController.cs
@@ -6,9 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float forwardSpeed = 70.0f;
     private Vector3 direction;
+    private Vector3 hitDirection;
     private float forwardInput;
+    private float powerupStrength = 15.0f;
+    public bool hasPowerup;
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    public GameObject powerupIndicator;
 
 
     // Start is called before the first frame update
@@ -24,5 +28,34 @@ public class PlayerController : MonoBehaviour
         direction = focalPoint.transform.forward;
         forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(forwardInput * forwardSpeed * direction);
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            powerupIndicator.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupTimer());
+        }
+    }
+
+    IEnumerator PowerupTimer()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Debug.Log("Collided with " + collision.gameObject.name + " with powerup set to " + hasPowerup);
+            hitDirection = (collision.transform.position - transform.position).normalized;
+            collision.rigidbody.AddForce(powerupStrength * hitDirection, ForceMode.Impulse);
+        }
     }
 }

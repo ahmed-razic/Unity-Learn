313ed6616b4ad5e97e270a2c5999d56582ab0809
Ahmed Razic <ahmed.razic@gmail.com>
2024-02-04 12:03:53 +0100

    ClickeMouse - Added restart button

--- JuniorProgrammer/CreateWithCode2/ClickyMouse/Assets/Scripts/GameManager.cs
+++ JuniorProgrammer/CreateWithCode2/ClickyMouse/Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,6 +14,8 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI gameOverText;
     private int score = 0;
     public bool isGameActive;
+
+    public Button restartButton;
 
     // Start is called before the first frame update
     void Start()
@@ -42,5 +46,12 @@ public class GameManager : MonoBehaviour
     {
         gameOverText.gameObject.SetActive(true);
         isGameActive = false;
+        restartButton.gameObject.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("Clicky Mouse");
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }

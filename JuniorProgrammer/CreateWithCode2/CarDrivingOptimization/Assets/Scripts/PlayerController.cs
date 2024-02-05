90b4f6d238d3f85c96198e744d30fb3f694f154c
Ahmed Razic <ahmed.razic@gmail.com>
2024-02-05 06:12:09 -0800

    CarDrivingOptimization - Added wheel colliders, speedometer and rpm

--- JuniorProgrammer/CreateWithCode2/CarDrivingOptimization/Assets/Scripts/PlayerController.cs
+++ JuniorProgrammer/CreateWithCode2/CarDrivingOptimization/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
@@ -9,11 +10,18 @@ public class PlayerController : MonoBehaviour
     public float horsePower = 1000f;
     public float turnPower = 50f;
     private Rigidbody playerRb;
+    [SerializeField] private GameObject centerOfMass;
+
+    [SerializeField] private TextMeshProUGUI speedometerText;
+    [SerializeField] private TextMeshProUGUI rpmText;
+    [SerializeField] private float speed;
+    [SerializeField] private float rpm;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = centerOfMass.transform.position;
     }
 
     // Update is called once per frame
@@ -21,5 +29,11 @@ public class PlayerController : MonoBehaviour
     {
         playerRb.AddRelativeForce(horsePower * Input.GetAxis("Vertical") * Vector3.forward);
         transform.Rotate(Vector3.up, turnPower * Input.GetAxis("Horizontal") * Time.deltaTime);
+
+        speed = Mathf.Round(playerRb.velocity.magnitude * 3.6f); // 2.237f for mph
+        speedometerText.SetText("Speed: " + speed + " kph");
+
+        rpm = Mathf.Round((speed % 30) * 40);
+        rpmText.SetText("RPM: " + rpm);
     }
 }

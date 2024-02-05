62fe3d33aac56276c53b1dffa6d77b4d5bb84f3b
Ahmed Razic <ahmed.razic@gmail.com>
2024-02-05 06:26:13 -0800

    CarDrivingOptimization - Added check to see if all wheels are on the ground

--- JuniorProgrammer/CreateWithCode2/CarDrivingOptimization/Assets/Scripts/PlayerController.cs
+++ JuniorProgrammer/CreateWithCode2/CarDrivingOptimization/Assets/Scripts/PlayerController.cs
@@ -17,6 +17,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float rpm;
 
+    [SerializeField] private int wheelsOnGround;
+    [SerializeField] private List<WheelCollider> allWheels;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,15 +28,39 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        playerRb.AddRelativeForce(horsePower * Input.GetAxis("Vertical") * Vector3.forward);
-        transform.Rotate(Vector3.up, turnPower * Input.GetAxis("Horizontal") * Time.deltaTime);
+        if (IsOnGround())
+        {
+            playerRb.AddRelativeForce(horsePower * Input.GetAxis("Vertical") * Vector3.forward);
+            transform.Rotate(Vector3.up, turnPower * Input.GetAxis("Horizontal") * Time.deltaTime);
 
-        speed = Mathf.Round(playerRb.velocity.magnitude * 3.6f); // 2.237f for mph
-        speedometerText.SetText("Speed: " + speed + " kph");
+            speed = Mathf.Round(playerRb.velocity.magnitude * 3.6f); // 2.237f for mph
+            speedometerText.SetText("Speed: " + speed + " kph");
 
-        rpm = Mathf.Round((speed % 30) * 40);
-        rpmText.SetText("RPM: " + rpm);
+            rpm = Mathf.Round((speed % 30) * 40);
+            rpmText.SetText("RPM: " + rpm);
+        }
+    }
+
+    bool IsOnGround()
+    {
+        wheelsOnGround = 0;
+        foreach (WheelCollider wheel in allWheels)
+        {
+            if (wheel.isGrounded)
+            {
+                wheelsOnGround++;
+            }
+        }
+
+        if (wheelsOnGround == 4)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }

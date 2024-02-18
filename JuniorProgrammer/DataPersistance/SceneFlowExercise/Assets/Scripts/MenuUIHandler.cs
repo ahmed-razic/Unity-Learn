e9e2abd57f57c6b2456e153f92daf9a2bdf1eef5
Ahmed Razic <ahmed.razic@gmail.com>
2024-02-18 12:01:24 +0100

    SceneFlowExercise

---
+++ JuniorProgrammer/DataPersistance/SceneFlowExercise/Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuUIHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartSceneOne()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+    public void StartSceneTwo()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
